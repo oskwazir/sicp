5d8d4f3dd45ac79f3256a273119f89b07d891e3c
Omer Wazir <omer.wazir@gmail.com>
2015-07-12 00:39:25 -0700

    Formated differently

--- exercises/chapter1/exercise1-2.rkt
+++ exercises/chapter1/exercise1-2.rkt
@@ -1,4 +1,14 @@
 #lang racket
 ; SICP ex1.2
 ; translate 5 + 4 + (2 - (3 - (6 + 4/5))) / 3(6-2)(2-7)
-(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5 ))))) (* 3 (- 6 2) (- 2 7)))
+;Not entirely sure the best way to format multiple nested expressions
+(/
+ (+ 5 4
+    (- 2
+       (- 3
+          (+ 6
+             (/ 4 5)))))
+ (* 3
+    (- 6 2)
+    (- 2 7)))
+ 
