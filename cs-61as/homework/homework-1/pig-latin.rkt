f234176960bc3b17cc4bd103e4f5094b31b0e032
Omer Wazir <omer.wazir@gmail.com>
2015-07-15 00:03:55 -0700

    Made the has-vowels? function better

--- cs-61as/homework/homework-1/pig-latin.rkt
+++ cs-61as/homework/homework-1/pig-latin.rkt
@@ -4,19 +4,27 @@
 
 (define (vowel? x) (member x '(a e i o u)))
 
-(define (has-vowels? wd shifted)
-  (cond ((= shifted (count wd)) #f)
+(define (has-vowels? wd)
+  (define (iterate-over-letters wd shifted)
+     (cond ((= shifted (count wd)) #f)
          ((vowel? (first wd)) #t)
          (else
-          (has-vowels?
+          (iterate-over-letters
            (word (bf wd) (first wd)) (+ 1 shifted)))))
+  (iterate-over-letters wd 0))
     
 (define (pigl wd)
   ; If the input word has no vowels just return the word directly.
-  (if (not (has-vowels? wd 0))
+  (if (not (has-vowels? wd))
       (word wd)
 
   ;If the input word starts with a consonant then we move all the starting consonants to the end of the word and then append "ay" to the end.
       (cond ((not (vowel? (first wd))) (pigl (word (bf wd) (first wd))))
          ;If the input word starts with a vowel then we append "ay" to the input.
          (else (word wd 'ay)))))
+
+(pigl 'scheme)
+(pigl 'hello)
+(pigl 'my)
+(pigl 'apple)
+(pigl 'colorado)
